d1a60d25a5ad88ddd8379a09f2b2d9a908084f9d
sophiesoe <sophieamestina@gmail.com>
2023-06-26 17:43:06 +0630

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,43 @@ const waLone = localFont({
 
 export const metadata = {
   title: "Ar Luu Messenger",
-  description: "messenger talk chat conversation ar-luu potato",
+  keywords: ["messenger", "talk chat", "conversation", "ar-luu", "potato"],
+  description:
+    "A place where all your words should be laid on without distraction",
+  url: "https://ar-luu-messenger.vercel.app/",
+  openGraph: {
+    images: [
+      {
+        url: "https://ar-luu-messenger.vercel.app/images/arr-luu.png",
+        width: 800,
+        height: 600,
+        alt: "Ar Luu Messenger",
+      },
+      {
+        url: "https://ar-luu-messenger.vercel.app/images/arr-luu.png",
+        width: 1800,
+        height: 1600,
+        alt: "Ar Luu Messenger",
+      },
+    ],
+  },
+  twitter: {
+    cardType: "summary_large_image",
+    images: [
+      {
+        url: "https://ar-luu-messenger.vercel.app/images/arr-luu.png",
+        width: 800,
+        height: 600,
+        alt: "Ar Luu Messenger",
+      },
+      {
+        url: "https://ar-luu-messenger.vercel.app/images/arr-luu.png",
+        width: 1800,
+        height: 1600,
+        alt: "Ar Luu Messenger",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
