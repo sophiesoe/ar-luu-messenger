244929b938f3d8f7a0cf857813c8c5ab2de1c44f
sophiesoe <sophieamestina@gmail.com>
2023-06-26 16:36:47 +0630

    bug fix

--- app/libs/pusher.ts
+++ app/libs/pusher.ts
@@ -13,7 +13,7 @@ export const pusherClient = new PusherClient(
   process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   {
     channelAuthorization: {
-      endpoint: "/pages/api/pusher/auth",
+      endpoint: "/api/pusher/auth",
       transport: "ajax",
     },
     cluster: "ap3",
